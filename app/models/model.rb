ce8303c4895a3f5e20b05f53e5d0fb751daf9767
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-17 18:18:20 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,21 @@
-class Model
-  # Replace with Dog clas
+class Dog 
+  @@all=[] 
+  attr_accessor :name, :breed, :age
+  def initialize(name,breed,age)
+    @name=name
+    @breed=breed
+    @age=age
+    @@all << name
+  end
+  def self.all
+    return @@all
+  end
+  
 end
+# Create a new file in the models directory to create a dog class. This class
+# should have name, breed, and age attributes which can be set on initialization.
+# You should be able to read and write to these attributes. This class should
+# also keep track of each instance of dog created, as well as a class method
+# `all` to return an array of those instances.
+
+# gret=Dog.new("gret","red","5")
