c591e43bbb17c945ecd225a9b49e832cb1f0ae8e
christinacarlson <christina.carlson213@gmail.com>
2018-07-17 18:20:11 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,16 @@
-class Model
-  # Replace with Dog clas
+class Dog
+
+attr_accessor :name, :breed, :age
+@@all=[]
+
+  def initialize(name, breed, age)
+    @name=name
+    @breed=breed
+    @age=age
+  @@all << name
+  end
+   def self.all 
+     return @@all
+   end
 end
+# sparky=Dog.new("rudolph", "mastiff", 2)
